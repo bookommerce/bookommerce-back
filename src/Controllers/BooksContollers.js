151131f333a0ca9185ce744aa958fa9051db1bca
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-01-28 13:43:29 -0300

    feat: add /getBooks & /getCategories endpoint

--- src/Controllers/BooksContollers.js
+++ src/Controllers/BooksContollers.js
@@ -1,9 +1,22 @@
 import db from "../Config/database.js";
 
-export async function getBooks(_, res) {
+export async function getBooks(req, res) {
+    const {category} = req.headers;
+
+    try {
+        const books = await db.collection("books").find(category && { Category: category } ).toArray();
+        return res.status(200).send(books);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getCategories(_, res) {
     try {
         const books = await db.collection("books").find().toArray();
-        return res.status(200).send(books);
+        const categories = [...new Set(books.map(b => b.Category))];
+        return res.status(200).send(categories);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
--- src/Routes/BooksRoutes.js
+++ src/Routes/BooksRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getBooks } from "../Controllers/BooksContollers.js";
+import { getBooks, getCategories } from "../Controllers/BooksContollers.js";
 
 
 const BooksRouter = Router();
 
 BooksRouter.get("/books", getBooks);
+
+BooksRouter.get("/categories", getCategories);
 
 export default BooksRouter;
