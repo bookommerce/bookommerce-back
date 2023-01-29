d9879cabef41b7226e10ee60826e5d8a2f3ba480
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-01-29 10:22:36 -0300

    refactor: implement a different route for each book request

--- src/Routes/BooksRoutes.js
+++ src/Routes/BooksRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getBooks, getCategories } from "../Controllers/BooksContollers.js";
-
+import { getBooks, getCategories, getBookById, getBooksByCategory } from "../Controllers/BooksControllers.js";
 
 const BooksRouter = Router();
 
 BooksRouter.get("/books", getBooks);
+BooksRouter.get("/books/category", getBooksByCategory);
+BooksRouter.get("/book/id", getBookById);
+
 
 BooksRouter.get("/categories", getCategories);
 
